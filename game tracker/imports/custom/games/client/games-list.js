b24b466cd0c45be5e33c78ec6b7c752ce19e1a32
fgokdata <75901506+fgokdata@users.noreply.github.com>
2023-05-08 16:29:42 +0200

    Add files via upload
    créé un boîte de dialogue modale à utiliser pour ajouter une nouvelle session

--- game tracker/imports/custom/games/client/games-list.js
+++ game tracker/imports/custom/games/client/games-list.js
@@ -2,6 +2,8 @@ import { Games } from '../../../startup/lib/collection';
 import './games-list.html';
 import '../../../../public/stylesheets/games-list.css';
 
+let modalTemplateInst = null;
+
 Template.gamesList.onCreated(function(){
     Meteor.subscribe('getGamesList')
 })
@@ -10,4 +12,20 @@ Template.gamesList.helpers({
     gamesList() {
         return Games.find().fetch();
     }
+})
+
+Template.gamesList.events({
+    'click .add-game-session-btn'(event, template) {
+        modalTemplateInst = Blaze.renderWithData(Template.sessionModal, { id: this.id }, document.getElementById('modal-parent'));
+    },
+})
+
+Template.sessionModal.events({
+    'click .close-modal'(event, template) {
+        event.preventDefault();
+        if(modalTemplateInst) {
+            Blaze.remove(modalTemplateInst);
+            modalTemplateInst = null;
+        }
+    },
 })
